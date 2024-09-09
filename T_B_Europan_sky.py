e12688bd35c6b90721598ae9bfdeff59063f3af7
Jake Sahli <jasa8100@colorado.edu>
2024-09-09 17:59:03 -0600

    Cleaned code for Europa sky, and transmission/reflection coefficients and made enclosing python files

---
+++ T_B_Europan_sky.py
@@ -0,0 +1,282 @@
+# Imports and plotting setups
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy
+import math
+
+import math_funcs
+
+from scipy.constants import (
+    epsilon_0,   # Permittivity of free space (vacuum)
+    mu_0,        # Permeability of free space (vacuum)
+    c,           # Speed of light in vacuum
+    #e,           # Elementary charge
+    #h,           # Planck constant
+    #hbar,        # Reduced Planck constant (h-bar)
+    k,           # Boltzmann constant
+    #G,           # Newtonian constant of gravitation
+    #m_e,         # Electron mass
+    #m_p,         # Proton mass
+    #m_n,         # Neutron mass
+    #alpha,       # Fine-structure constant
+    eV,          # Electron volt
+)
+
+#Define coldspace, jovian, gal radiation field for a mission 
+T_B_J_low_band = 10**11 #K
+T_B_J_high_band = 10**2 # K
+T_B_gal_low_band = 10**7 # K
+T_B_gal_high_band = T_B_J_high_band
+T_B_coldspace = 2.7 #K
+
+# Define a function to resample to the 1 deg find 
+# grid to 10 deg course grid that the antenna pattern is using
+class angle_grid:
+    def __init__(self, dtheta, dphi):
+        self.dtheta = dtheta
+        self.dphi = dphi
+
+        self.theta_resample = np.arange(0, 180+dtheta, dtheta, dtype=int) # deg
+        self.phi_resample = np.arange(0, 360+dphi, dphi, dtype=int) # deg
+
+        self.phi_grid_resample, \
+            self.theta_grid_resample = \
+                np.meshgrid(self.phi_resample, self.theta_resample)
+        
+    def get_angle_grid(self):
+        return self.theta_resample, self.phi_resample, \
+            self.phi_grid_resample, self.theta_grid_resample
+
+def resample_course(T_B, return_grids=False):
+    # Define the course theta and phi sampling grid
+    theta_resample, phi_resample, \
+        phi_grid_resample, theta_grid_resample = angle_grid(10, 10).get_angle_grid()
+
+    # Resample and reweight the sampling grid points
+    T_B_resample = np.zeros_like(theta_grid_resample, dtype=float).flatten()
+    for i in np.arange(theta_grid_resample.size):
+        theta = theta_grid_resample.flatten()[i]
+        phi = phi_grid_resample.flatten()[i]
+
+        theta_range = (theta + np.arange(-5, 5, 1)) % 180
+        phi_range = (phi + np.arange(-5, 5, 1)) % 360
+        dsteradian = 100
+        for sub_theta in theta_range:
+            for sub_phi in phi_range:
+                T_B_resample[i] += T_B[sub_theta, sub_phi]
+
+        T_B_resample[i] = T_B_resample[i] / dsteradian
+
+    T_B_resample = T_B_resample.reshape(theta_grid_resample.shape)
+
+    if return_grids:
+        return T_B_resample, theta_resample, phi_resample, theta_grid_resample, phi_grid_resample
+    else:
+        return T_B_resample
+
+# Brightness Field For the SubJovian point
+def T_B_sub_jovian_point():
+    # Define the theta and phi fine sampling grid
+    theta, phi, \
+        phi_grid, theta_grid  = angle_grid(1, 1).get_angle_grid()
+
+    # Define the brightness grid with vertical and horizontal polarization
+    T_Bv_low_band = np.zeros_like(theta_grid, dtype=float).flatten()
+    T_Bh_low_band = np.zeros_like(theta_grid, dtype=float).flatten()
+    T_Bv_high_band = np.zeros_like(theta_grid, dtype=float).flatten()
+    T_Bh_high_band = np.zeros_like(theta_grid, dtype=float).flatten()
+
+    for i in np.arange(theta_grid.size):
+        theta = theta_grid.flatten()[i]
+        phi = phi_grid.flatten()[i]
+        
+        # Addition of cold space background everywhere
+        T_Bv_low_band[i] += 0.5 * T_B_coldspace
+        T_Bh_low_band[i] += 0.5 * T_B_coldspace
+        T_Bv_high_band[i] += 0.5 * T_B_coldspace
+        T_Bh_high_band[i] += 0.5 * T_B_coldspace
+
+        # Draw the galactic background
+        if theta >= 105 and theta <= 135:
+            if phi >= 180 and phi <= 360:
+                T_Bv_low_band[i] += 0.5 * T_B_gal_low_band
+                T_Bh_low_band[i] += 0.5 * T_B_gal_low_band
+                T_Bv_high_band[i] += 0.5 * T_B_gal_high_band
+                T_Bh_high_band[i] += 0.5 * T_B_gal_high_band
+
+        # DAM radiation illumination 
+        # from aural regions of jupiter visible only 1 degree
+        # And only at the low band
+        if theta == 6:
+            if phi == 0 or phi == 180:
+                T_Bv_low_band[i] += 0.5 * T_B_J_low_band
+                T_Bh_low_band[i] += 0.5 * T_B_J_low_band
+
+        # DIM radiation illumination 
+        # from jupiter itself directly
+        # Only really visible in the high band
+        if theta <= 6:
+            T_Bv_high_band[i] += 0.5 * T_B_J_high_band
+            T_Bh_high_band[i] += 0.5 * T_B_J_high_band
+
+    T_Bv_low_band = resample_course(T_Bv_low_band.reshape(theta_grid.shape))
+    T_Bh_low_band = resample_course(T_Bh_low_band.reshape(theta_grid.shape))
+    T_Bv_high_band = resample_course(T_Bv_high_band.reshape(theta_grid.shape))
+    T_Bh_high_band = resample_course(T_Bh_high_band.reshape(theta_grid.shape))
+
+    return T_Bv_low_band, T_Bh_low_band, T_Bv_high_band, T_Bh_high_band
+
+# Brightness Field For the AntiJovian point
+def T_B_anti_jovian_point():
+    # Define the theta and phi fine sampling grid
+    theta, phi, \
+        phi_grid, theta_grid  = angle_grid(1, 1).get_angle_grid()
+
+    # Define the brightness grid with vertical and horizontal polarization
+    T_Bv_low_band = np.zeros_like(theta_grid, dtype=float).flatten()
+    T_Bh_low_band = np.zeros_like(theta_grid, dtype=float).flatten()
+    T_Bv_high_band = np.zeros_like(theta_grid, dtype=float).flatten()
+    T_Bh_high_band = np.zeros_like(theta_grid, dtype=float).flatten()
+
+    for i in np.arange(theta_grid.size):
+        theta = theta_grid.flatten()[i]
+        phi = phi_grid.flatten()[i]
+        
+        # Addition of cold space background everywhere
+        T_Bv_low_band[i] += 0.5 * T_B_coldspace
+        T_Bh_low_band[i] += 0.5 * T_B_coldspace
+        T_Bv_high_band[i] += 0.5 * T_B_coldspace
+        T_Bh_high_band[i] += 0.5 * T_B_coldspace
+
+        # Draw the galactic background
+        if theta >= 45 and theta <= 75:
+            if phi >= 0 and phi <= 180:
+                T_Bv_low_band[i] += 0.5 * T_B_gal_low_band
+                T_Bh_low_band[i] += 0.5 * T_B_gal_low_band
+                T_Bv_high_band[i] += 0.5 * T_B_gal_high_band
+                T_Bh_high_band[i] += 0.5 * T_B_gal_high_band
+
+        # DAM radiation illumination 
+        # from aural regions of jupiter visible only 1 degree
+        # And only at the low band
+        if theta == 174:
+            if phi == 0 or phi == 180:
+                T_Bv_low_band[i] += 0.5 * T_B_J_low_band
+                T_Bh_low_band[i] += 0.5 * T_B_J_low_band
+
+        # DIM radiation illumination 
+        # from jupiter itself directly
+        # Only really visible in the high band
+        if theta >= 174:
+                T_Bv_high_band[i] += 0.5 * T_B_J_high_band
+                T_Bh_high_band[i] += 0.5 * T_B_J_high_band
+
+    T_Bv_low_band = resample_course(T_Bv_low_band.reshape(theta_grid.shape))
+    T_Bh_low_band = resample_course(T_Bh_low_band.reshape(theta_grid.shape))
+    T_Bv_high_band = resample_course(T_Bv_high_band.reshape(theta_grid.shape))
+    T_Bh_high_band = resample_course(T_Bh_high_band.reshape(theta_grid.shape))
+
+    return T_Bv_low_band, T_Bh_low_band, T_Bv_high_band, T_Bh_high_band
+
+# Brightness Field For the AntiOrbital point
+def T_B_anti_orbital_point():
+    # Define the theta and phi fine sampling grid
+    theta, phi, \
+        phi_grid, theta_grid  = angle_grid(1, 1).get_angle_grid()
+
+    # Define the brightness grid with vertical and horizontal polarization
+    T_Bv_low_band = np.zeros_like(theta_grid, dtype=float).flatten()
+    T_Bh_low_band = np.zeros_like(theta_grid, dtype=float).flatten()
+    T_Bv_high_band = np.zeros_like(theta_grid, dtype=float).flatten()
+    T_Bh_high_band = np.zeros_like(theta_grid, dtype=float).flatten()
+
+    for i in np.arange(theta_grid.size):
+        theta = theta_grid.flatten()[i]
+        phi = phi_grid.flatten()[i]
+        
+        # Addition of cold space background everywhere
+        T_Bv_low_band[i] += 0.5 * T_B_coldspace
+        T_Bh_low_band[i] += 0.5 * T_B_coldspace
+        T_Bv_high_band[i] += 0.5 * T_B_coldspace
+        T_Bh_high_band[i] += 0.5 * T_B_coldspace
+
+        # Draw the galactic background
+        if theta >= 15 and theta <= 45:
+            if phi >= 0 and phi <= 180:
+                T_Bv_low_band[i] += 0.5 * T_B_gal_low_band
+                T_Bh_low_band[i] += 0.5 * T_B_gal_low_band
+                T_Bv_high_band[i] += 0.5 * T_B_gal_high_band
+                T_Bh_high_band[i] += 0.5 * T_B_gal_high_band
+
+        # DAM radiation illumination 
+        # from aural regions of jupiter visible only 1 degree
+        # And only at the low band
+        if theta == 96 or theta == 84:
+            if phi == 270:
+                T_Bv_low_band[i] += 0.5 * T_B_J_low_band
+                T_Bh_low_band[i] += 0.5 * T_B_J_low_band
+
+        # DIM radiation illumination 
+        # from jupiter itself directly
+        # Only really visible in the high band
+        if theta >= 84 and theta <= 96:
+            if phi >= 264 and phi <= 276:
+                T_Bv_high_band[i] += 0.5 * T_B_J_high_band
+                T_Bh_high_band[i] += 0.5 * T_B_J_high_band
+
+    T_Bv_low_band = resample_course(T_Bv_low_band.reshape(theta_grid.shape))
+    T_Bh_low_band = resample_course(T_Bh_low_band.reshape(theta_grid.shape))
+    T_Bv_high_band = resample_course(T_Bv_high_band.reshape(theta_grid.shape))
+    T_Bh_high_band = resample_course(T_Bh_high_band.reshape(theta_grid.shape))
+
+    return T_Bv_low_band, T_Bh_low_band, T_Bv_high_band, T_Bh_high_band
+
+if __name__ == "__main__":
+    for func, title in [
+        (T_B_sub_jovian_point, 'Sub-Jovian Point'), 
+        (T_B_anti_jovian_point, 'Anti-Jovian Point'),
+        (T_B_anti_orbital_point, 'Anti-Orbital Point')]:
+
+        T_Bv_low_band, T_Bh_low_band, \
+            T_Bv_high_band, T_Bh_high_band \
+             = func()
+        
+        # Set the global theta and phi to the course grid
+        theta, phi, \
+            phi_grid, theta_grid = angle_grid(10, 10).get_angle_grid()
+
+        theta_grid_rad = np.deg2rad(theta_grid)
+        phi_grid_rad = np.deg2rad(phi_grid)
+
+        # Plot only the Northern Hemisphere of the brightness temperature
+        U = (np.sin(theta_grid_rad[0:9]) * np.cos(phi_grid_rad[0:9])).flatten()
+        V = (np.sin(theta_grid_rad[0:9]) * np.sin(phi_grid_rad[0:9])).flatten()
+
+        plt.figure()
+        plt.scatter(U, V, c=10 * np.log10(T_Bv_low_band.reshape(theta_grid_rad.shape)[0:9].flatten())
+            , cmap='viridis', vmin=0, vmax=110)
+        plt.colorbar(label='Brightness Temperature (dBK)')
+        plt.xlabel('U')
+        plt.ylabel('V')
+        plt.title('Northern UV Projection of Vertical Brightness Temperature Low Band')
+        plt.suptitle(title)
+        plt.savefig(title + ' Northern UV Projection of Vertical Brightness Temperature Low Band.pdf')
+
+        # Plot only the Northern Hemisphere of the brightness temperature
+        U = (np.sin(theta_grid_rad[0:9]) * np.cos(phi_grid_rad[0:9])).flatten()
+        V = (np.sin(theta_grid_rad[0:9]) * np.sin(phi_grid_rad[0:9])).flatten()
+
+        plt.figure()
+        plt.scatter(U, V, c=10 * np.log10(T_Bv_high_band.reshape(theta_grid_rad.shape)[0:9].flatten())
+            , cmap='viridis', vmin=0, vmax=70)
+        plt.colorbar(label='Brightness Temperature (dBK)')
+        plt.xlabel('U')
+        plt.ylabel('V')
+        plt.title('Northern UV Projection of Vertical Brightness Temperature High Band')
+        plt.suptitle(title)
+        plt.savefig(title + ' Northern UV Projection of Vertical Brightness Temperature High Band.pdf')
+
+
+
---
+++ transmission_reflection_coefficients.py
@@ -0,0 +1,366 @@
+import numpy as np
+from scipy.constants import (
+    epsilon_0,   # Permittivity of free space (vacuum)
+    mu_0,        # Permeability of free space (vacuum)
+    c,           # Speed of light in vacuum
+)
+from scipy.special import loggamma, factorial, gamma
+from decimal import Decimal
+import decimal
+
+def transmissivity_fBm(
+        f, epsilon_i_prime, epsilon_t_prime, sigma_A, H, 
+        theta_i, theta_t, phi_t, bistatic_polarization,
+        tolerance = 1e-5,
+        decimal_precision = int(1e3),
+        max_n_convergence = 10.0,
+        debug = False
+        ):
+    # Giorgio Franceschetti, Daniele Riccio, 
+    # “Chapter 6 - Scattering from Fractional 
+    # Brownian Surfaces: Physical-Optics Solution” 
+    # in Scattering, Natural Surfaces, and Fractals, 2006
+
+    # Evaluate constants
+    lambda_i = c / (f * np.sqrt(epsilon_i_prime)) 
+    lambda_t = c / (f * np.sqrt(epsilon_t_prime)) 
+    k_i = 2 * np.pi / lambda_i
+    k_t = 2 * np.pi / lambda_t
+
+    eta_xy = np.sqrt((k_t * np.sin(theta_t) * np.cos(phi_t) - k_i * np.sin(theta_i))**2 \
+                    + (k_t * np.sin(theta_t) * np.sin(phi_t))**2)
+    eta_z = k_i * np.cos(theta_i) - k_t * np.cos(theta_t)
+
+    # Evaluate the fBm series
+    sum, terms_to_converge, accuracy_overflow = _evaluate_fBm_series(
+        sigma_A, H, 
+        eta_xy, eta_z,
+        tolerance,
+        decimal_precision,
+        max_n_convergence,
+        debug
+    )
+
+    ## Calculate the transmissivity
+    eta_i = np.sqrt(mu_0 / (epsilon_i_prime * epsilon_0))
+    eta_t = np.sqrt(mu_0 / (epsilon_t_prime * epsilon_0))
+
+    N_ab = bistatic_transmission_coefficients(
+        epsilon_i_prime, epsilon_t_prime, theta_i, 
+        theta_t, phi_t, bistatic_polarization
+        )
+
+    gamma_ab_t = (k_t**2 / np.cos(theta_i)) * (eta_i / eta_t) * np.abs(N_ab)**2 * (1 / (2*H)) * float(sum)
+
+    if debug:
+        return gamma_ab_t, sum, terms_to_converge, accuracy_overflow
+    else:
+        return gamma_ab_t
+    
+
+def reflectivity_fBm(
+        f, epsilon_i_prime, epsilon_t_prime, sigma_A, H, 
+        theta_i, theta_s, phi_s, bistatic_polarization,
+        tolerance = 1e-5,
+        decimal_precision = int(1e3),
+        max_n_convergence = 10.0,
+        debug = False
+        ):
+    # Giorgio Franceschetti, Daniele Riccio, 
+    # “Chapter 6 - Scattering from Fractional 
+    # Brownian Surfaces: Physical-Optics Solution” 
+    # in Scattering, Natural Surfaces, and Fractals, 2006
+
+    # Evaluate constants
+    lambda_i = c / (f * np.sqrt(epsilon_i_prime)) 
+    k_i = 2 * np.pi / lambda_i
+
+    eta_xy = np.sqrt((k_i * np.sin(theta_s) * np.cos(phi_s) - k_i * np.sin(theta_i))**2 \
+                    + (k_i * np.sin(theta_s) * np.sin(phi_s))**2)
+    eta_z = k_i * np.cos(theta_i) + k_i * np.cos(theta_s)
+
+    # Evaluate the fBm series
+    sum, terms_to_converge, accuracy_overflow = _evaluate_fBm_series(
+        sigma_A, H, 
+        eta_xy, eta_z,
+        tolerance,
+        decimal_precision,
+        max_n_convergence,
+        debug
+    )
+
+    ## Calculate the reflectivity
+    F_ab = bistatic_scattering_coefficients(
+        epsilon_i_prime, epsilon_t_prime, theta_i, 
+        theta_s, phi_s, bistatic_polarization)
+
+    gamma_ab_s = (k_i**2 / np.cos(theta_i)) * np.abs(F_ab)**2 * (1 / (2*H)) * float(sum)
+
+    if debug:
+        return gamma_ab_s, sum, terms_to_converge, accuracy_overflow
+    else:
+        return gamma_ab_s
+
+def fresnel_coefficients(n_i, n_t, theta_i):
+    R_v = (n_i * np.cos(theta_i) - n_t * np.sqrt(1 - (np.sin(theta_i) * n_i / n_t)**2)) \
+        / (n_i * np.cos(theta_i) + n_t * np.sqrt(1 - (np.sin(theta_i) * n_i / n_t)**2))
+
+    R_h = (n_i * np.sqrt(1 - (n_i * np.sin(theta_i) / n_t)**2) - n_t * np.cos(theta_i)) \
+        / (n_i * np.sqrt(1 - (n_i * np.sin(theta_i) / n_t)**2) + n_t * np.cos(theta_i))
+
+    return R_v, R_h
+
+def bistatic_scattering_coefficients(
+    epsilon_i_prime, epsilon_t_prime, theta_i, 
+    theta_s, phi_s, bistatic_polarization):
+    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    # in Theory of Microwave Remote Sensing, 1985.
+
+    n_i = np.sqrt(epsilon_i_prime * epsilon_0 * mu_0)
+    n_t = np.sqrt(epsilon_t_prime * epsilon_0 * mu_0)
+
+    R_v, R_h = fresnel_coefficients(n_i, n_t, theta_i)
+
+    if bistatic_polarization == 'hh':
+        F_ab = ((1 - R_h) * np.cos(theta_i) - (1 + R_h) * np.cos(theta_s)) * np.cos(phi_s)
+    elif bistatic_polarization == 'hv':
+        F_ab = ((1 + R_h) - (1 - R_h) * np.cos(theta_i) * np.cos(theta_s)) * np.sin(phi_s)
+    elif bistatic_polarization == 'vh':
+        F_ab = ((1 + R_v) * np.cos(theta_i) * np.cos(theta_s) - (1 - R_v)) * np.sin(phi_s)
+    elif bistatic_polarization == 'vv':
+        F_ab = (-1 * (1 - R_v) * np.cos(theta_s) + (1 + R_v) * np.cos(theta_i)) * np.cos(phi_s)
+    else:
+        raise(ValueError('bistatic polarization must be \'vv\',  \'vh\',  \'hv\',  or \'hh\''))
+    
+    return F_ab
+
+def bistatic_transmission_coefficients(
+    epsilon_i_prime, epsilon_t_prime, theta_i, 
+    theta_t, phi_t, bistatic_polarization):
+    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    # in Theory of Microwave Remote Sensing, 1985.
+
+    eta_i = np.sqrt(mu_0 / (epsilon_i_prime * epsilon_0))
+    eta_t = np.sqrt(mu_0 / (epsilon_t_prime * epsilon_0))
+    n_i = np.sqrt(epsilon_i_prime * epsilon_0 * mu_0)
+    n_t = np.sqrt(epsilon_t_prime * epsilon_0 * mu_0)
+
+    R_v, R_h = fresnel_coefficients(n_i, n_t, theta_i)
+
+    if bistatic_polarization == 'hh':
+        N_ab = ((1 + R_h) * np.cos(theta_t) + (eta_t / eta_i) * (1 - R_h) * np.cos(theta_i)) * np.cos(phi_t)
+    elif bistatic_polarization == 'hv':
+        N_ab = (-1 * (1 + R_h) - (eta_t / eta_i) * (1 - R_h) * np.cos(theta_i) * np.cos(theta_t)) * np.sin(phi_t)
+    elif bistatic_polarization == 'vh':
+        N_ab = ((eta_t / eta_i) * (1 + R_v) - (1 - R_v) * np.cos(theta_i) * np.cos(theta_t)) * np.sin(phi_t)
+    elif bistatic_polarization == 'vv':
+        N_ab = ((eta_t / eta_i) * (1 + R_v) + (1 - R_v) * np.cos(theta_i)) * np.cos(phi_t)
+    else:
+        raise(ValueError('bistatic polarization must be \'vv\',  \'vh\',  \'hv\',  or \'hh\''))
+
+    return N_ab
+
+def _evaluate_fBm_series(
+        sigma_A, H, 
+        eta_xy, eta_z,
+        tolerance = 1e-5,
+        decimal_precision = int(1e3),
+        max_n_convergence = 10.0,
+        debug = False):
+    # Giorgio Franceschetti, Daniele Riccio, 
+    # “Chapter 6 - Scattering from Fractional 
+    # Brownian Surfaces: Physical-Optics Solution” 
+    # in Scattering, Natural Surfaces, and Fractals, 2006
+
+    if H < 0.5:
+        raise(ValueError('This series is only valid for H >= 1/2'))
+
+    # Evaluate the series S at 0
+    n = 0
+
+    first_fraction = ((-1) ** n) / (2**(2*n) * (factorial(n))**2)
+    second_fraction = (eta_xy)**(2*n) / (0.5 * eta_z**2 * sigma_A**2)**((n+1)/H)
+    third_factor = gamma((n + 1) / H)
+
+    sum = Decimal(first_fraction*second_fraction*third_factor)
+    delta_sum_list = [np.log(first_fraction*second_fraction*third_factor)]
+    old_log_factorial = np.log(factorial(n))
+    accuracy_overflow = False
+
+    # Evaluate the series S from 1 to max_n_convergence
+    # or until the terms is less than tolerance
+    for n in np.arange(1.0, max_n_convergence + 1.0, 1):
+
+        new_log_factorial = 2 * np.log(n) + old_log_factorial
+        old_log_factorial = new_log_factorial
+        kernel =  -2 * n * np.log(2) \
+            - new_log_factorial \
+                + 2 * n * np.log(eta_xy) \
+                    - ((n+1) / H)*np.log(0.5 * eta_z**2 * sigma_A**2) \
+                        + loggamma((n + 1) / H)
+        
+        delta_sum = Decimal(np.e)**Decimal(kernel)
+        sum += Decimal((-1**n)) * delta_sum
+        delta_sum_list.append(kernel)
+
+        if kernel > decimal_precision :
+            accuracy_overflow = True
+
+        if n != 0:   
+            if tolerance > delta_sum:
+                break
+    
+    # Mask the sum if it exceeded the max n convergence, 
+    # or if the decimal precision was not high enough
+    terms_to_converge = n
+
+    if terms_to_converge >= max_n_convergence:
+        sum = np.nan
+        
+    if accuracy_overflow:
+        sum = np.nan
+
+    return sum, terms_to_converge, accuracy_overflow
+
+def transmissivity_gaussian(
+    f, epsilon_i_prime, epsilon_t_prime, sigma_h, correlation_length, 
+    theta_i, theta_t, phi_t, bistatic_polarization,
+    tolerance = 1e-5,
+    decimal_precision = int(1e3),
+    max_n_convergence = 10.0,
+    debug = False
+    ):
+    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    # in Theory of Microwave Remote Sensing, 1985.
+
+    lambda_i = c / (f * np.sqrt(epsilon_i_prime)) 
+    lambda_t = c / (f * np.sqrt(epsilon_t_prime)) 
+    k_i = 2 * np.pi / lambda_i
+    k_t = 2 * np.pi / lambda_t
+
+    eta_xy = np.sqrt((k_t * np.sin(theta_t) * np.cos(phi_t) - k_i * np.sin(theta_i))**2 \
+                    + (k_t * np.sin(theta_t) * np.sin(phi_t))**2)
+    eta_z = k_i * np.cos(theta_i) - k_t * np.cos(theta_t)
+
+    # Evaluate the fBm series
+    sum, terms_to_converge, accuracy_overflow = _evaluate_gaussian_series(
+        sigma_h, correlation_length, 
+        eta_xy, eta_z,
+        tolerance,
+        decimal_precision,
+        max_n_convergence,
+        debug
+    )
+
+    ## Calculate the transmissivity
+    eta_i = np.sqrt(mu_0 / (epsilon_i_prime * epsilon_0))
+    eta_t = np.sqrt(mu_0 / (epsilon_t_prime * epsilon_0))
+
+    N_ab = bistatic_transmission_coefficients(
+        epsilon_i_prime, epsilon_t_prime, theta_i, 
+        theta_t, phi_t, bistatic_polarization
+        )
+
+    gamma_ab_t = (k_t**2 / (4 * np.cos(theta_i))) \
+        * (eta_i / eta_t) * np.abs(N_ab)**2 * float(sum)
+
+    if debug:
+        return gamma_ab_t, sum, terms_to_converge, accuracy_overflow
+    else:
+        return gamma_ab_t
+
+def reflectivity_gaussian(
+    f, epsilon_i_prime, epsilon_t_prime, sigma_h, correlation_length, 
+    theta_i, theta_s, phi_s, bistatic_polarization,
+    tolerance = 1e-5,
+    decimal_precision = int(1e3),
+    max_n_convergence = 10.0,
+    debug = False
+    ):
+    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    # in Theory of Microwave Remote Sensing, 1985.
+
+    # Evaluate constants
+    lambda_i = c / (f * np.sqrt(epsilon_i_prime)) 
+    k_i = 2 * np.pi / lambda_i
+
+    eta_xy = np.sqrt((k_i * np.sin(theta_s) * np.cos(phi_s) - k_i * np.sin(theta_i))**2 \
+                    + (k_i * np.sin(theta_s) * np.sin(phi_s))**2)
+    eta_z = k_i * np.cos(theta_i) + k_i * np.cos(theta_s)
+
+    # Evaluate the fBm series
+    sum, terms_to_converge, accuracy_overflow = _evaluate_gaussian_series(
+        sigma_h, correlation_length, 
+        eta_xy, eta_z,
+        tolerance,
+        decimal_precision,
+        max_n_convergence,
+        debug
+    )
+
+    ## Calculate the reflectivity
+    F_ab = bistatic_scattering_coefficients(
+        epsilon_i_prime, epsilon_t_prime, theta_i, 
+        theta_s, phi_s, bistatic_polarization)
+    
+    gamma_ab_s = (k_i**2 / (4 * np.cos(theta_i))) \
+        * np.abs(F_ab)**2 * float(sum)
+
+    if debug:
+        return gamma_ab_s, sum, terms_to_converge, accuracy_overflow
+    else:
+        return gamma_ab_s
+
+def _evaluate_gaussian_series(
+        sigma_h, correlation_length,  
+        eta_xy, eta_z,
+        tolerance = 1e-5,
+        decimal_precision = int(1e3),
+        max_n_convergence = 10.0,
+        debug = False):
+    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    # in Theory of Microwave Remote Sensing, 1985.
+
+    sum = Decimal(0)
+    delta_sum_list = []
+    old_log_factorial = np.log(factorial(n))
+    accuracy_overflow = False
+
+    # Evaluate the series S from 1 to max_n_convergence
+    # or until the terms is less than tolerance
+    for n in np.arange(1.0, max_n_convergence + 1.0, 1):
+        new_log_factorial = np.log(n) + old_log_factorial
+        old_log_factorial = new_log_factorial
+        
+        kernel =  -1 * new_log_factorial + np.log(correlation_length**2 / n) \
+            + n * np.log(eta_z**2 * sigma_h**2) \
+                - eta_xy **2 * correlation_length**2 / (4 * n) \
+                    + sigma_h**2 * eta_z**2
+        
+        delta_sum = Decimal(np.e)**Decimal(kernel)
+        sum += delta_sum
+        delta_sum_list.append(kernel)
+        
+        if kernel > decimal_precision :
+            accuracy_overflow = True
+
+        if n != 0:   
+            if tolerance > delta_sum:
+                break
+    
+    # Mask the sum if it exceeded the max n convergence, 
+    # or if the decimal precision was not high enough
+    terms_to_converge = n
+
+    if terms_to_converge >= max_n_convergence:
+        sum = np.nan
+        
+    if accuracy_overflow:
+        sum = np.nan
+
+    return sum, terms_to_converge, accuracy_overflow
