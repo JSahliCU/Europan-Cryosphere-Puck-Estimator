fc7509e418316ad2c880eca44a86bfe07a852598
Jake Sahli <jasa8100@colorado.edu>
2024-10-08 14:45:12 -0600

    Added comments to transmission and reflection coefficients

--- transmission_reflection_coefficients.py
+++ transmission_reflection_coefficients.py
@@ -5,11 +5,8 @@ from scipy.constants import (
     c,           # Speed of light in vacuum
 )
 from scipy.special import loggamma, factorial, gamma
-# from math import gamma
-# from math import lgamma as loggamma
 from decimal import Decimal
 import sys
-# from numba import jit
 
 def transmissivity_fBm(
         f, epsilon_i_prime, epsilon_t_prime, sigma_A, H, 
@@ -19,10 +16,35 @@ def transmissivity_fBm(
         max_n_convergence = 10.0,
         debug = False
         ):
-    # Giorgio Franceschetti, Daniele Riccio, 
-    # “Chapter 6 - Scattering from Fractional 
-    # Brownian Surfaces: Physical-Optics Solution” 
-    # in Scattering, Natural Surfaces, and Fractals, 2006
+    """
+    Based on the method presented by Franceschetti and Riccio for scattering from fractional brownian surfaces
+    This function calculates the transmissivity coefficient for an fBm 
+    surface described by 2D allan variance, Hurst coefficient at the passedbistatic polarization
+
+    Giorgio Franceschetti, Daniele Riccio, 
+    “Chapter 6 - Scattering from Fractional 
+    Brownian Surfaces: Physical-Optics Solution” 
+    in Scattering, Natural Surfaces, and Fractals, 2006
+
+    Args:
+        f (float): Frequency
+        epsilon_i_prime (float): permittivity of the incident medium
+        epsilon_t_prime (float): permittivity of the medium the wave is transmitted to
+        sigma_A (float): 2D Allan variance of the surface
+        H (float): Hurst exponent
+        theta_i (float): incident theta angle
+        theta_t (float): transmission theta angle
+        phi_t (float): transmission phi angle
+        bistatic_polarization (str): 'vv', 'hv', 'vh' or 'hh'
+        tolerance (float, optional): Convergence criteria: If the difference between subsequent 
+            series elements is less than tolerance, the series is assumed to converge. Defaults to 1e-5.
+        decimal_precision (int, optional): The amount of decimal precision to use for the series elements. Defaults to int(1e3).
+        max_n_convergence (float, optional): Maximum number of series elemnts. Defaults to 10.0.
+        debug (bool, optional): Enables debug mode. Defaults to False.
+
+    Returns:
+        float: transmissivity coefficient for an fBm surface described by sigma_A, H at the bistatic polarization
+    """
 
     # Evaluate constants
     lambda_i = c / (f * np.sqrt(epsilon_i_prime)) 
@@ -69,11 +91,35 @@ def reflectivity_fBm(
         max_n_convergence = 10.0,
         debug = False
         ):
-    # Giorgio Franceschetti, Daniele Riccio, 
-    # “Chapter 6 - Scattering from Fractional 
-    # Brownian Surfaces: Physical-Optics Solution” 
-    # in Scattering, Natural Surfaces, and Fractals, 2006
+    """
+    Based on the method presented by Franceschetti and Riccio for scattering from fractional brownian surfaces
+    This function calculates the reflectivity coefficient for an fBm 
+    surface described by 2D allan variance, Hurst coefficient at the passedbistatic polarization
 
+    Giorgio Franceschetti, Daniele Riccio, 
+    “Chapter 6 - Scattering from Fractional 
+    Brownian Surfaces: Physical-Optics Solution” 
+    in Scattering, Natural Surfaces, and Fractals, 2006
+
+    Args:
+        f (float): Frequency
+        epsilon_i_prime (float): permittivity of the incident medium
+        epsilon_t_prime (float): permittivity of the scattering medium
+        sigma_A (float): 2D Allan variance of the surface
+        H (float): Hurst exponent
+        theta_i (float): incident theta angle
+        theta_t (float): scattering theta angle
+        phi_t (float): scattering phi angle
+        bistatic_polarization (str): 'vv', 'hv', 'vh' or 'hh'
+        tolerance (float, optional): Convergence critera: If the difference between 
+            subsequent series elements is less than tolerance, the series is assumed to converge. Defaults to 1e-5.
+        decimal_precision (int, optional): The amount of decimal precision to use for the series elements. Defaults to int(1e3).
+        max_n_convergence (float, optional): Maximum number of series elemnts. Defaults to 10.0.
+        debug (bool, optional): Enables debug mode. Defaults to False.
+
+    Returns:
+        float: reflectivity coefficient for an fBm surface described by sigma_A, H at the bistatic polarization
+    """
     # Evaluate constants
     lambda_i = c / (f * np.sqrt(epsilon_i_prime)) 
     k_i = 2 * np.pi / lambda_i
@@ -111,6 +157,16 @@ def reflectivity_fBm(
         return gamma_ab_s
 
 def fresnel_coefficients(n_i, n_t, theta_i):
+    """Calculation for fresnel reflection coefficients
+
+    Args:
+        n_i (float): index of refrection of incident medium
+        n_t (float): index of refrection of the scattering medium
+        theta_i (float): incident angle of the wave front measured from the normal of medium interface
+
+    Returns:
+        float, float: Vertical and horizontal fresnel coefficients, respectively
+    """
     sin_theta_t = n_i * np.sin(theta_i) / n_t
     # Case of total internal reflection
     if sin_theta_t > 1:
@@ -128,9 +184,22 @@ def fresnel_coefficients(n_i, n_t, theta_i):
 def bistatic_scattering_coefficients(
     epsilon_i_prime, epsilon_t_prime, theta_i, 
     theta_s, phi_s, bistatic_polarization):
-    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
-    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
-    # in Theory of Microwave Remote Sensing, 1985.
+    """
+    Bistatic fresnel scattering coefficients
+    Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    in Theory of Microwave Remote Sensing, 1985.
+    Args:
+        epsilon_i_prime (float): permittivity of the incident medium
+        epsilon_t_prime (float): permittivity of the scattering medium
+        theta_i (float): incident angle of the wave front measured from the normal of medium interface
+        theta_s (float): scattering theta angle of the wave front measured from the normal of medium interface
+        phi_s (float): scattering phi angle of the wave front measured from incident phi angle
+        bistatic_polarization (str): 'vv', 'hv', 'vh' or 'hh'
+
+    Returns:
+        float : bistatic fresnel scattering coefficients
+    """
 
     n_i = np.sqrt(epsilon_i_prime * epsilon_0 * mu_0)
     n_t = np.sqrt(epsilon_t_prime * epsilon_0 * mu_0)
@@ -153,10 +222,22 @@ def bistatic_scattering_coefficients(
 def bistatic_transmission_coefficients(
     epsilon_i_prime, epsilon_t_prime, theta_i, 
     theta_t, phi_t, bistatic_polarization):
-    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
-    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
-    # in Theory of Microwave Remote Sensing, 1985.
+    """
+    Bistatic Fresnel transmission coefficients
+    Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    in Theory of Microwave Remote Sensing, 1985.
+    Args:
+        epsilon_i_prime (float): permittivity of the incident medium
+        epsilon_t_prime (float): permittivity of the transmission medium
+        theta_i (float): incident angle of the wave front measured from the normal of medium interface
+        theta_t (float): transmission theta angle of the wave front measured from the normal of medium interface
+        phi_t (float): transmission phi angle of the wave front measured from incident phi angle
+        bistatic_polarization (str): 'vv', 'hv', 'vh' or 'hh'
 
+    Returns:
+        float : bistatic Fresnel scattering coefficients
+    """
     eta_i = np.sqrt(mu_0 / (epsilon_i_prime * epsilon_0))
     eta_t = np.sqrt(mu_0 / (epsilon_t_prime * epsilon_0))
     n_i = np.sqrt(epsilon_i_prime * epsilon_0 * mu_0)
@@ -177,7 +258,6 @@ def bistatic_transmission_coefficients(
 
     return N_ab
 
-# @jit
 def _evaluate_fBm_series(
         sigma_A, H, 
         eta_xy, eta_z,
@@ -185,10 +265,35 @@ def _evaluate_fBm_series(
         decimal_precision = int(1e3),
         max_n_convergence = 10.0,
         debug = False):
-    # Giorgio Franceschetti, Daniele Riccio, 
-    # “Chapter 6 - Scattering from Fractional 
-    # Brownian Surfaces: Physical-Optics Solution” 
-    # in Scattering, Natural Surfaces, and Fractals, 2006
+    
+    """
+    Based on the method presented by Franceschetti and Riccio for scattering 
+    from fractional brownian surfaces
+    This function calculates the series for a persistent fBm 
+    surface described by 2D allan variance, Hurst coefficient at the 
+    passed bistatic polarization
+
+    Currently only supports persistent fBm
+
+    Giorgio Franceschetti, Daniele Riccio, 
+    “Chapter 6 - Scattering from Fractional 
+    Brownian Surfaces: Physical-Optics Solution” 
+    in Scattering, Natural Surfaces, and Fractals, 2006
+
+    Args:
+        sigma_A (float): 2D Allan variance of the surface
+        H (float): Hurst exponent
+        eta_xy (float): wave vector in the xy direction
+        eta_z (float): wave vector in the z direction
+        tolerance (float, optional): Convergence criteria: If the difference between 
+            subsequent series elements is less than tolerance, the series is assumed to converge. Defaults to 1e-5.
+        decimal_precision (int, optional): The amount of decimal precision to use for the series elements. Defaults to int(1e3).
+        max_n_convergence (float, optional): Maximum number of series elements. Defaults to 10.0.
+        debug (bool, optional): Enables debug mode. Defaults to False.
+
+    Returns:
+        float: fBm Series output
+    """
 
     if H < 0.5:
         raise(ValueError('This series is only valid for H >= 1/2'))
@@ -254,9 +359,29 @@ def transmissivity_gaussian(
     max_n_convergence = 10.0,
     debug = False
     ):
-    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
-    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
-    # in Theory of Microwave Remote Sensing, 1985.
+    """
+    Calculates the bistatic transmissivity coefficients of Gaussian Surface
+    Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    in Theory of Microwave Remote Sensing, 1985.
+    Args:
+        f (float): frequency to evaluate
+        epsilon_i_prime (float): permittivity of the incident medium
+        epsilon_t_prime (float): permittivity of the transmission medium
+        sigma_h (float): variance of the surface height
+        correlation_length (float): correlation length of surface
+        theta_i (float): incident angle
+        theta_t (float): transmitted angle
+        phi_t (float): transmitted phi angle, measured from the incident phi angle
+        bistatic_polarization (float): Can be 'vv', 'vh', 'hv', or 'hh'
+        tolerance (float, optional): Tolerance to terminate series. Defaults to 1e-5.
+        decimal_precision (int, optional): Number of digits in series evaluation. Defaults to int(1e3).
+        max_n_convergence (float, optional): Maximum number of series terms before termination. Defaults to 10.0.
+        debug (bool, optional): Turns on debug information. Defaults to False.
+
+    Returns:
+        float: bistatic transmissivity coefficient of gaussian surface
+    """
 
     lambda_i = c / (f * np.sqrt(epsilon_i_prime)) 
     lambda_t = c / (f * np.sqrt(epsilon_t_prime)) 
@@ -302,9 +427,30 @@ def reflectivity_gaussian(
     max_n_convergence = 10.0,
     debug = False
     ):
-    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
-    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
-    # in Theory of Microwave Remote Sensing, 1985.
+    """
+    Calculates the bistatic reflectivity coefficients of Gaussian Surface
+    Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    in Theory of Microwave Remote Sensing, 1985.
+
+    Args:
+        f (float): frequency to evaluate
+        epsilon_i_prime (float): permittivity of the incident medium
+        epsilon_t_prime (float): permittivity of the transmission medium
+        sigma_h (float): variance of the surface height
+        correlation_length (float): correlation length of surface
+        theta_i (float): incident angle
+        theta_s (float): scattered angle
+        phi_s (float): scattered angle phi measured from the incident angle phi
+        bistatic_polarization (float): Can be 'vv', 'vh', 'hv', or 'hh'
+        tolerance (float, optional): Tolerance to terminate series. Defaults to 1e-5.
+        decimal_precision (int, optional): Number of digits in series evaluation. Defaults to int(1e3).
+        max_n_convergence (float, optional): Maximum number of series terms before termination. Defaults to 10.0.
+        debug (bool, optional): Turns on debug information. Defaults to False.
+
+    Returns:
+        float: bistatic reflectivity coefficient of gaussian surface
+    """
 
     # Evaluate constants
     lambda_i = c / (f * np.sqrt(epsilon_i_prime)) 
@@ -344,9 +490,26 @@ def _evaluate_gaussian_series(
         decimal_precision = int(1e3),
         max_n_convergence = 10.0,
         debug = False):
-    # Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
-    # “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
-    # in Theory of Microwave Remote Sensing, 1985.
+    """
+    Evaluates the series for the Gaussian surface 
+    calculation of transmission and reflection coefficients
+    Tsang, Leung, Kong, Jin Au, Shin, Robert T. 
+    “Ch 2.6. Scattering and Emission by Random Rough Surfaces” 
+    in Theory of Microwave Remote Sensing, 1985.
+
+    Args:
+        sigma_h (float): variance of the surface height
+        correlation_length (float): correlation length of surface
+        eta_xy (float): wave vector in the xy plane
+        eta_z (float): wave vector in the z direction
+        tolerance (float, optional): Tolerance to terminate series. Defaults to 1e-5.
+        decimal_precision (int, optional): Number of digits in series evaluation. Defaults to int(1e3).
+        max_n_convergence (float, optional): Maximum number of series terms before termination. Defaults to 10.0.
+        debug (bool, optional): Turns on debug information. Defaults to False.
+
+    Returns:
+        float: result of series
+    """
 
     sum = Decimal(0)
     delta_sum_list = []
